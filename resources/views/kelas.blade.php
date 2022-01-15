e9966520d02a82bd11913230fe7bcfe5451f62b5
Rizki idham sofyan <rizki99sofyan@gmail.com>
2022-01-15 14:28:23 +0700

    nambah titik

--- resources/views/kelas.blade.php
+++ resources/views/kelas.blade.php
@@ -121,7 +121,7 @@
                             <div class="card-body">
                                 <div class="alert alert-danger">
                                     <h5 class="card-title">Success card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content..</p>
                                 </div>
         
                             </div>
