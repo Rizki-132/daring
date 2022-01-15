33d10852b84b38680d2a197cb29c21107849ef7a
Rizki idham sofyan <rizki99sofyan@gmail.com>
2022-01-15 14:25:39 +0700

    komit pertama

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title') Ujian-Online</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <!-- bootstrap 3.0.2 -->
+        <link href="{{asset("template/css/bootstrap.min.css")}}" rel="stylesheet" type="text/css" />
+        <!-- font Awesome -->
+        <link href="{{asset("template/css/font-awesome.min.css")}}" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="{{asset("template/css/ionicons.min.css")}}" rel="stylesheet" type="text/css" />
+        <!-- Morris chart -->
+        <link href="{{asset("template/css/morris/morris.css")}}" rel="stylesheet" type="text/css" />
+        <!-- jvectormap -->
+        <link href="{{asset("template/css/jvectormap/jquery-jvectormap-1.2.2.css")}}" rel="stylesheet" type="text/css" />
+        <!-- fullCalendar -->
+        <link href="{{asset("template/css/fullcalendar/fullcalendar.css")}}" rel="stylesheet" type="text/css" />
+        <!-- Daterange picker -->
+        <link href="{{asset("template/css/daterangepicker/daterangepicker-bs3.css")}}" rel="stylesheet" type="text/css" />
+        <!-- bootstrap wysihtml5 - text editor -->
+        <link href="{{asset("template/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="{{asset("template/css/AdminLTE.css")}}" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="skin-black">
+        <!-- header logo: style can be found in header.less -->
+        
+            <!-- Header Navbar: style can be found in header.less -->
+        
+               @yield('dashboard')
+                
+               @yield('content')
+               
+
+        <!-- add new calendar event modal -->
+
+
+        <!-- jQuery 2.0.2 -->
+        @yield('script')     
+
+    </body>
+</html>
